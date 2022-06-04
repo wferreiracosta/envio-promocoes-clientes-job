893743ac74f55008bf4eb197098944ac494a8f21
Wesley Costa <wesferrcosta@gmail.com>
2022-06-03 21:18:15 -0300

    Processador para gerar emails para cada cliente

---
+++ src/main/java/br/com/wferreiracosta/enviopromocoesclientes/processors/ProcessarEmailProdutoClienteProcessor.java
@@ -0,0 +1,33 @@
+package br.com.wferreiracosta.enviopromocoesclientes.processors;
+
+import br.com.wferreiracosta.enviopromocoesclientes.domains.InteresseProdutoCliente;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Component;
+
+import static java.lang.String.format;
+import static java.text.NumberFormat.getCurrencyInstance;
+
+@Component
+public class ProcessarEmailProdutoClienteProcessor implements ItemProcessor<InteresseProdutoCliente, SimpleMailMessage> {
+
+    @Override
+    public SimpleMailMessage process(InteresseProdutoCliente interesseProdutoCliente) throws Exception {
+        final var email = new SimpleMailMessage();
+        email.setFrom("xpto@no-reply.com");
+        email.setTo(interesseProdutoCliente.getCliente().getEmail());
+        email.setSubject("Promoção Imperdivel!!!!");
+        email.setText(gerarTextoPromocao(interesseProdutoCliente));
+        return email;
+    }
+
+    private String gerarTextoPromocao(InteresseProdutoCliente interesseProdutoCliente) {
+        final var writer = new StringBuilder();
+        writer.append(format("Olá, %s!\n\n", interesseProdutoCliente.getCliente().getNome()));
+        writer.append("Essa promoção pode ser do seu interesse:\n\n");
+        writer.append(format("%s - %s\n\n", interesseProdutoCliente.getProduto().getNome(), interesseProdutoCliente.getProduto().getDescricao()));
+        writer.append(format("Por apenas: %s!", getCurrencyInstance().format(interesseProdutoCliente.getProduto().getPreco())));
+        return writer.toString();
+    }
+
+}
